26bf5980553b9b344bff63225d36feaf253f680d
wansaleh <wansaleh@gmail.com>
2016-01-21 11:27:25 +0800

    Initial.

---
+++ app/assets/scripts/main.js
@@ -0,0 +1,219 @@
+!function($) {
+  'use strict';
+
+  var SITE = function() {
+    this.VERSION = "1.0.0";
+    this.Pages = $.Pages;
+  };
+
+  SITE.prototype.initSlabtext = function() {
+    var fonts = [
+      'LeagueGothic',
+      'Playfair Display'
+    ];
+
+    $('[data-slabtext]').slabText();
+
+    $.each(fonts, function(index, font) {
+      fontSpy(font, {
+        success: function () {
+          $('[data-slabtext]').slabText();
+        }
+      });
+    });
+  }
+
+  SITE.prototype.initMisc = function() {
+    $('[data-fitvids]').fitVids();
+
+    // $('body:not(.mobile)').find('h1, h2, h3, h4, h5, h6, p').filter(':not(.no-orphan)').unorphanize();
+    $('body:not(.mobile)').find('p').filter(':not(.no-orphan)').unorphanize();
+    $('body:not(.mobile) .orphan').unorphanize();
+
+    $('[data-bullet]').each(function () {
+      var bullet = $(this).data('bullet');
+      $(this).find('li').each(function () {
+        $(this).prepend('<i class="' + bullet + '"></i>');
+      });
+    });
+
+    $('.social-icon i[class^=icon-]').each(function() {
+      $(this).clone().addClass($(this).attr('class') + '-cloned').appendTo($(this).parent());
+    });
+  };
+
+  SITE.prototype.initSwiper = function() {
+    var $vimeo = $('#penyelamat');
+
+    //Intialize Slider
+    var swiper = new Swiper('#hero', {
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      parallax: true,
+      speed: 1000,
+      onSlideChangeStart: function(sw) {
+        if (sw.activeIndex == 2) {
+          $vimeo.vimeo('play');
+        }
+        else {
+          $vimeo.vimeo('pause');
+        }
+
+        if ($.Pages.getUserAgent() === 'desktop') {
+          $vimeo.appear();
+
+          $vimeo.on('appear', function() {
+            if (sw.activeIndex == 2) {
+              $vimeo.vimeo('play');
+            }
+          });
+          $vimeo.on('disappear', function() {
+            if (sw.activeIndex == 2) {
+              $vimeo.vimeo('pause');
+            }
+          });
+        }
+      }
+    });
+
+    $('.watchvid').click(function(ev) {
+      ev.preventDefault();
+      swiper.slideTo(2);
+    });
+  };
+
+  SITE.prototype.initForm = function() {
+    var $contactForm = $('#contact-form');
+    $contactForm.validate({
+      submitHandler: function(form) {
+        $contactForm.find('.alert').remove();
+
+        $.ajax({
+          url: '//formspree.io/newmedia@hafizhamidun.com',
+          method: 'POST',
+          data: $contactForm.serialize(),
+          dataType: 'json',
+
+          success: function(data) {
+            $contactForm.append('<div class="alert alert-success fade in" role="alert"><button class="close" data-dismiss="alert"></button><i class="fa fa-check"></i> <strong>Success</strong> Your message has been sent. Thank you.</div>');
+          },
+
+          error: function(err) {
+            $contactForm.append('<div class="alert alert-error fade in" role="alert"><button class="close" data-dismiss="alert"></button><i class="fa fa-times"></i> <strong>Error</strong> Please correct the errors above.</div>');
+          }
+        });
+      },
+
+      invalidHandler: function(event, validator) {
+        console.log(validator.numberOfInvalids());
+        $contactForm.find('.alert').remove();
+        $contactForm.append('<div class="alert alert-error fade in" role="alert"><button class="close" data-dismiss="alert"></button><i class="fa fa-times"></i> <strong>Invalid</strong> Please correct the errors above.</div>');
+      }
+    });
+  };
+
+  SITE.prototype.initSocial = function () {
+    var nFormatter = function(num, digits) {
+      var si = [
+        { value: 1E18, symbol: "E" },
+        { value: 1E15, symbol: "P" },
+        { value: 1E12, symbol: "T" },
+        { value: 1E9,  symbol: "G" },
+        { value: 1E6,  symbol: "M" },
+        { value: 1E3,  symbol: "k" }
+      ], i;
+      for (i = 0; i < si.length; i++) {
+        if (num >= si[i].value) {
+          return (num / si[i].value).toFixed(digits).replace(/\.?0+$/, "") + si[i].symbol;
+        }
+      }
+      return num;
+    };
+
+    $('.c-facebook').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/facebook?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.likes, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+
+    $('.c-twitter').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/twitter?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.followers_count, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+
+    $('.c-instagram').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/instagram?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.data.counts.followed_by, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+
+    $('.c-youtube').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/youtube?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.items[0].statistics.subscriberCount, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+
+    $('.c-spotify').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/spotify?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.followers.total, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+
+    $('.c-soundcloud').each(function () {
+      $.getJSON('//wms-api.herokuapp.com/hh/soundcloud?callback=?', function(data) {
+        $('.count', this).html(nFormatter(data.followers_count, 1));
+        $(this).addClass('loaded');
+      }.bind(this));
+    });
+  };
+
+  SITE.prototype.initInstagramFeed = function() {
+     $(".instagram-feed").each(function () {
+      var $this = $(this);
+
+      $.getJSON('//wms-api.herokuapp.com/hh/instagram/feed?callback=?', function(data) {
+        $(data.data).each(function (index, feed) {
+          if (index < 6) {
+            var item = $('\
+              <div class="grid col-md-2 col-sm-4 col-xs-6">\
+                <a href="' + feed.link + '" target="_blank">\
+                  <img src="' + feed.images.standard_resolution.url + '" alt="">\
+                  <span class="counts">\
+                    <i class="ti-heart"></i> ' + feed.likes.count + '&nbsp;\
+                    <i class="ti-comment"></i> ' + feed.comments.count + '\
+                  </span>\
+                </a>\
+              </div>');
+            $this.append(item);
+          }
+        });
+      });
+    });
+  };
+
+  SITE.prototype.init = function() {
+    FastClick.attach(document.body);
+    // this.initSlabtext();
+    this.initMisc();
+    // this.initSwiper();
+    // this.initSocial();
+    // this.initInstagramFeed();
+    // this.initForm();
+  };
+
+  $.Pages.SITE = new SITE();
+  $.Pages.SITE.Constructor = SITE;
+
+  $(function() {
+    $.Pages.init();
+    $.Pages.SITE.init();
+  });
+}(window.jQuery);
---
+++ app/assets/scripts/music.js
@@ -0,0 +1,231 @@
+!function($) {
+
+  $(".music-isotope").each(function() {
+    var $this = $(this);
+
+    function applyIsotope() {
+      $this.isotope({
+        itemSelector: ".grid",
+        percentPosition: true,
+        layoutMode: "masonry",
+        masonry: {
+          columnWidth: ".grid-sizer"
+        },
+
+        getSortData: {
+          title: '[data-title]',
+          released: '[data-released]'
+        },
+        sortBy: 'released',
+        sortAscending: {
+          title: true,
+          released: false
+        },
+
+        transitionDuration: '.8s',
+        hiddenStyle: {
+          transform: 'translateY(-100%)',
+          opacity: 0
+        },
+        visibleStyle: {
+          transform: 'translateY(0)',
+          opacity: 1
+        }
+      });
+      setTimeout(function() {
+        $this.isotope("layout");
+      });
+
+      $this.on('layoutComplete', function( event, filteredItems ) {
+        $this.addClass('loaded');
+      });
+
+      $('.sort-button-group').on('click', 'button', function() {
+        var sortValue = $(this).data('sortValue');
+        console.log(sortValue);
+        $this.isotope({ sortBy: sortValue });
+      });
+
+      $('.sort-button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+          $buttonGroup.find('.active').removeClass('active');
+          $( this ).addClass('active');
+        });
+      });
+
+      $('.filter-button-group').on('click', 'button', function() {
+        var filterValue = $(this).data('filterValue');
+        $this.isotope({ filter: filterValue });
+      });
+
+      $('.filter-button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+          $buttonGroup.find('.active').removeClass('active');
+          $( this ).addClass('active');
+        });
+      });
+    }
+
+    function applyColors() {
+      // convert 0..255 R,G,B values to a hexidecimal color string
+      var RGBToHex = function(r,g,b){
+        var bin = r << 16 | g << 8 | b;
+        return (function(h){
+          return new Array(7-h.length).join("0")+h
+        })(bin.toString(16).toUpperCase())
+      }
+
+      // convert a hexidecimal color string to 0..255 R,G,B
+      var hexToRGB = function(hex){
+        hex = parseInt(hex, 16);
+        var r = hex >> 16;
+        var g = hex >> 8 & 0xFF;
+        var b = hex & 0xFF;
+        return [r,g,b];
+      }
+
+      var rgba = function(color, alpha) {
+        alpha = parseFloat(alpha) || 1;
+
+        return "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", " + alpha + ")"
+      }
+
+      var yiq = function(color) {
+        var rgb = rgba(color).match(/\d+/g);
+        return ((rgb[0]*299)+(rgb[1]*587)+(rgb[2]*114))/1000;
+      }
+
+      var getDarkestColor = function(palette) {
+        var darkestColor = palette[0], darkestLuma = 255, luma;
+        $.each(palette, function(index, color) {
+          luma = yiq(color);
+          if (luma < darkestLuma) {
+            darkestColor = color;
+            darkestLuma = luma;
+          }
+        });
+
+        return darkestColor;
+      }
+
+      var getLightestColor = function(palette) {
+        var lightestColor = palette[0], lightestLuma = 0, luma;
+        $.each(palette, function(index, color) {
+          luma = yiq(color);
+          if (luma > lightestLuma) {
+            lightestColor = color;
+            lightestLuma = luma;
+          }
+        });
+
+        return lightestColor;
+      }
+
+      var isDark = function(color) {
+        return yiq(color) <= 128;
+      }
+
+      var colorThief = new ColorThief();
+
+      $this.find(".grid").each(function() {
+        var bgColor = $(this).data('bg-color');
+        var fgColor = $(this).data('fg-color');
+
+        if (typeof bgColor !== 'undefined') {
+          bgColor = hexToRGB(bgColor);
+        }
+        if (typeof fgColor !== 'undefined') {
+          fgColor = hexToRGB(fgColor);
+        }
+
+        var $img = $(this).find("img");
+
+        var $info = $(this).find(".info");
+        var $infoBg = $(this).find(".info-bg");
+        var $label = $info.find(".label");
+
+        if (typeof bgColor === 'undefined') {
+          bgColor = colorThief.getColor($img[0]);
+        }
+
+        var palette = colorThief.getPalette($img[0]);
+        var p = $(this).data('palette');
+
+        if (typeof p !== 'undefined') {
+          bgColor = palette[parseInt(p)];
+        }
+
+        if (typeof fgColor === 'undefined') {
+          fgColor = isDark(bgColor) ? getLightestColor(palette) : getDarkestColor(palette);
+        }
+
+        $infoBg.css("background-color", rgba(bgColor));
+        $info.css("color", rgba(fgColor));
+        $label.css({
+          "border-color": rgba(fgColor),
+          "color": rgba(fgColor)
+        });
+
+        var $links = $(this).find(".links");
+        var $btn = $links.find(".btn");
+
+        $btn.css({
+          "background-color": rgba(fgColor),
+          "color": rgba(bgColor)
+        });
+
+      });
+    }
+
+    $this.waitForImages(true).done(function() {
+      applyIsotope();
+      applyColors();
+    });
+  });
+
+  var lightbox = function() {
+    var $lightbox = $('.video-lightbox');
+    var $video = $lightbox.find('.vlb-video');
+    var $close = $lightbox.find('.vlb-close');
+
+    $lightbox._open = function() {
+      $lightbox.addClass('vlb-visible');
+    }
+    $lightbox._close = function() {
+      $lightbox.removeClass('vlb-visible');
+      setTimeout(function() { $video.empty(); }, 500);
+    }
+    $close.click($lightbox._close);
+
+    $(document).on('keyup',function(evt) {
+      if (evt.keyCode == 27) {
+        $lightbox._close();
+      }
+    });
+
+    $lightbox.click(function (ev) {
+      $lightbox._close();
+    });
+
+    $('[data-youtube-embed]').each(function () {
+      var url = $(this).data('youtubeEmbed');
+
+      $(this).click(function (ev) {
+        $lightbox._open();
+
+        setTimeout(function () {
+          $video.html('<iframe width="560" height="315" src="' + url + '" frameborder="0" allowfullscreen></iframe>').fitVids();
+        }, 500);
+
+        ev.preventDefault();
+        ev.stopPropagation();
+      });
+
+    });
+  }
+
+  lightbox();
+
+}(window.jQuery);
